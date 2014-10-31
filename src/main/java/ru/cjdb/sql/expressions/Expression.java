9998198d37c04950f3defaa455ae7a74faee9dc6
Sergey Tselovalnikov <sergeicelov@gmail.com>
2014-10-31 11:38:47 +0300

    Where

---
+++ src/main/java/ru/cjdb/sql/expressions/Expression.java
@@ -0,0 +1,10 @@
+package ru.cjdb.sql.expressions;
+
+/**
+ * Какое-то выражение SQL
+ *
+ * @author Sergey Tselovalnikov
+ * @since 31.10.14
+ */
+public class Expression {
+}
---
+++ src/main/java/ru/cjdb/sql/expressions/conditions/Condition.java
@@ -0,0 +1,10 @@
+package ru.cjdb.sql.expressions.conditions;
+
+import ru.cjdb.sql.expressions.Expression;
+
+/**
+ * @author Sergey Tselovalnikov
+ * @since 31.10.14
+ */
+public class Condition extends Expression {
+}
