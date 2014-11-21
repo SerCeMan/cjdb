a43bdbdef58fec45d310b138ee916e11d686fe26
Dmitry Tishchenko <flir3d@gmail.com>
2014-11-21 19:28:39 +0300

    Added Varchar and Double tests, fixed PrintTest.

--- src/test/java/ru/cjdb/sql/QueryExecutorImplTest.java
+++ src/test/java/ru/cjdb/sql/QueryExecutorImplTest.java
@@ -80,22 +80,80 @@ public class QueryExecutorImplTest {
         String tableName = TestUtils.createRandomName();
         CreateTableQuery createTableQuery = new CreateTableQuery(tableName,
                 asList(new RowDefinition("column1", Types.INT),
-                        new RowDefinition("column2", Types.INT),
+                        new RowDefinition("column2", Types.varchar(4)),
                         new RowDefinition("column3", Types.INT)));
         queryExecutor.execute(createTableQuery);
 
-        InsertQuery insertQuery = new InsertQuery(tableName, 1, 2, 3);
+        InsertQuery insertQuery = new InsertQuery(tableName, 1, "str1", 3);
         queryExecutor.execute(insertQuery);
-        insertQuery = new InsertQuery(tableName, 2, 2, 3);
+        insertQuery = new InsertQuery(tableName, 2, "str2", 2);
         queryExecutor.execute(insertQuery);
-        insertQuery = new InsertQuery(tableName, 3, 2, 3);
+        insertQuery = new InsertQuery(tableName, 3, "str3", 1);
         queryExecutor.execute(insertQuery);
 
-        SelectQuery selectQuery = new SelectQuery(tableName, asList("test"));
+        SelectQuery selectQuery = new SelectQuery(tableName, asList("column1", "column2", "column3"));
         QueryResult queryResult = queryExecutor.execute(selectQuery);
 
         ResultPrinterImpl printer = new ResultPrinterImpl();
         printer.print(queryResult);
+    }
+
+    @Test
+    public void testCreateThenInsertThenSelectVarchar() {
+        String tableName = TestUtils.createRandomName();
+        CreateTableQuery createTableQuery = new CreateTableQuery(tableName,
+                asList(new RowDefinition("str1", Types.varchar(1)),
+                        new RowDefinition("str10", Types.varchar(10))));
+        queryExecutor.execute(createTableQuery);
+
+        InsertQuery insertQuery = new InsertQuery(tableName, "a", "abcdefghij");
+        queryExecutor.execute(insertQuery);
+        insertQuery = new InsertQuery(tableName, "k", "klmnopqrst");
+        queryExecutor.execute(insertQuery);
+
+        SelectQuery selectQuery = new SelectQuery(tableName, asList("str1", "str10"));
+        QueryResult queryResult = queryExecutor.execute(selectQuery);
+
+        Assert.assertTrue(queryResult.isSuccessful());
+        Assert.assertTrue(queryResult.hasResult());
+
+        Row row1 = queryResult.getCursor().nextRow();
+        Assert.assertEquals(row1.getColumnCount(), 2);
+        Assert.assertEquals(row1.getAt(0), "a");
+        Assert.assertEquals(row1.getAt(1), "abcdefghij");
+
+        Row row2 = queryResult.getCursor().nextRow();
+        Assert.assertEquals(row2.getAt(0),"k");
+        Assert.assertEquals(row2.getAt(1), "klmnopqrst");
+    }
+
+    @Test
+    public void testCreateThenInsertThenSelectDoubleType() {
+        String tableName = TestUtils.createRandomName();
+        CreateTableQuery createTableQuery = new CreateTableQuery(tableName,
+                asList(new RowDefinition("double1", Types.DOUBLE),
+                        new RowDefinition("constant", Types.DOUBLE)));
+        queryExecutor.execute(createTableQuery);
+
+        InsertQuery insertQuery = new InsertQuery(tableName, 1.0, 3.14);
+        queryExecutor.execute(insertQuery);
+        insertQuery = new InsertQuery(tableName, 2.0, 2.718281828459045);
+        queryExecutor.execute(insertQuery);
+
+        SelectQuery selectQuery = new SelectQuery(tableName, asList("double1", "constant"));
+        QueryResult queryResult = queryExecutor.execute(selectQuery);
+
+        Assert.assertTrue(queryResult.isSuccessful());
+        Assert.assertTrue(queryResult.hasResult());
+
+        Row row1 = queryResult.getCursor().nextRow();
+        Assert.assertEquals(row1.getColumnCount(), 2);
+        Assert.assertEquals(row1.getAt(0), 1.0);
+        Assert.assertEquals(row1.getAt(1), 3.14);
+
+        Row row2 = queryResult.getCursor().nextRow();
+        Assert.assertEquals(row2.getAt(0), 2.0);
+        Assert.assertEquals(row2.getAt(1), 2.718281828459045);
     }
 
     @Test
